b503af8aeba293aef957256dd8f50a85b6e0e1d0
BZ <ben.a.parsons@gmail.com>
2021-11-29 18:45:45 -0500

    Update helloworld to accept text inputs

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python 
 
+class Problem():
+    def __init__(self,filename):
+        names = []
+        with open(filename, "r") as file:
+            for line in file:
+                names.append(line)
+        return
+
 def main():
-    print("\n\n\nHello World!\n\n\n")
+    p = Problem('names.txt')
+    for name in p.names:
+        print("Hello {}!\n".format(name))
 
 if __name__ == "__main__":
     main()
